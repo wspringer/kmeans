13550da4e7198955111c11117d181973d9c35367
Wilfred Springer <wilfredspringer@gmail.com>
2012-06-23 15:57:58 +0200

    Cluster objects even if they don't exist in the address space yet.

--- src/main/scala/nl/flotsam/kmeans/KMeans.scala
+++ src/main/scala/nl/flotsam/kmeans/KMeans.scala
@@ -4,26 +4,34 @@ import annotation.tailrec
 
 object KMeans {
 
-  def cluster[T](xs: Seq[T], k: Int)(implicit g: Geometry[T]): Seq[Seq[T]] = {
+  def cluster[T,U](xs: Seq[T], k: Int)(implicit fn: T => U, g: Geometry[U]): Seq[Seq[T]] = {
+    case class Pair(original: T) {
+      val projected = fn(original)
+    }
     @tailrec
-    def step(xs: Seq[T], centroids: Seq[T]): Seq[Seq[T]] = {
+    def step(xs: Seq[Pair], centroids: Seq[U]): Seq[Seq[Pair]] = {
       val labeled =
         for (x <- xs) yield {
-          val distances = for ((centroid) <- centroids) yield (centroid, g.distance(x, centroid))
+          val distances = for ((centroid) <- centroids) yield (centroid, g.distance(x.projected, centroid))
           val nearestCentroid = distances.minBy(_._2)._1
           (nearestCentroid, x)
         }
       val grouped = for (centroid <- centroids) yield labeled.collect({
         case (`centroid`, x) => x
       })
-      val replacements = grouped.map(g.centroid)
+      val replacements = grouped.map(group => g.centroid(group.map(_.projected)))
       if (replacements.forall(replacement => centroids.exists(centroid => centroid == replacement))) {
         grouped
       } else {
         step(xs, replacements)
       }
     }
-    step(xs, xs.take(k))
+    val projectedXs = xs.map(Pair)
+    val groupedMappings = step(projectedXs, projectedXs.take(k).map(_.projected))
+    groupedMappings.map(_.map(_.original))
   }
+
+  def cluster[T,U](fn: T => U)(xs: Seq[T], k: Int)(implicit g: Geometry[U]): Seq[Seq[T]] =
+    cluster(xs, k)(fn, g)
 
 }
--- src/test/scala/nl/flotsam/kmeans/KMeansSpec.scala
+++ src/test/scala/nl/flotsam/kmeans/KMeansSpec.scala
@@ -8,18 +8,40 @@ import KMeans._
 class KMeansSpec extends Specification {
 
   "KMeans" should {
-    val points = List(
-      (1.0, 1.0),
-      (1.5, 2.0),
-      (3.0, 4.0),
-      (5.0, 7.0),
-      (3.5, 5.0),
-      (4.5, 5.0),
-      (3.5, 4.5)
-    )
-    val clustered = cluster(points, 2)
-    println(clustered)
-    clustered must not beNull
+
+    "allow you to cluster a list of points in a simple way" in {
+      val points = List(
+        (1.0, 1.0),
+        (1.5, 2.0),
+        (3.0, 4.0),
+        (5.0, 7.0),
+        (3.5, 5.0),
+        (4.5, 5.0),
+        (3.5, 4.5)
+      )
+      val clustered = cluster(points, 2)
+      println(clustered)
+      clustered must not beNull
+    }
+
+    "allow you to cluster a list of things that can be mapped to points in a simple way" in {
+      case class Coordinate(x: Double, y: Double)
+      val points = List(
+        Coordinate(1.0, 1.0),
+        Coordinate(1.5, 2.0),
+        Coordinate(3.0, 4.0),
+        Coordinate(5.0, 7.0),
+        Coordinate(3.5, 5.0),
+        Coordinate(4.5, 5.0),
+        Coordinate(3.5, 4.5)
+      )
+      def coordinateToPoint(coordinate: Coordinate) = (coordinate.x, coordinate.y)
+      val clustered = cluster(coordinateToPoint)(points, 2)
+      println(clustered)
+      clustered must not beNull
+    }
   }
+
+
 
 }
