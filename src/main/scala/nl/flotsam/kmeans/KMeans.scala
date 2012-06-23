1f46873e694bea45ada946ed9376d7b451522c67
Wilfred Springer <wilfredspringer@gmail.com>
2012-06-23 17:25:09 +0200

    Pick starting centroids randomly.

--- src/main/scala/nl/flotsam/kmeans/KMeans.scala
+++ src/main/scala/nl/flotsam/kmeans/KMeans.scala
@@ -1,37 +1,41 @@
 package nl.flotsam.kmeans
 
 import annotation.tailrec
+import util.Random
 
 object KMeans {
 
-  def cluster[T,U](xs: Seq[T], k: Int)(implicit fn: T => U, g: VectorSpace[U]): Seq[Seq[T]] = {
+  def pickRandom[T](xs: Seq[T], k: Int) = Random.shuffle(xs).take(k)
+
+  def cluster[T,U](xs: Seq[T], k: Int)
+                  (implicit projection: T => U, space: VectorSpace[U]): Seq[Seq[T]] = {
     case class Pair(original: T) {
-      val projected = fn(original)
+      val projected = projection(original)
     }
     @tailrec
     def step(xs: Seq[Pair], centroids: Seq[U]): Seq[Seq[Pair]] = {
       val labeled =
         for (x <- xs) yield {
-          val distances = for ((centroid) <- centroids) yield (centroid, g.distance(x.projected, centroid))
+          val distances = for ((centroid) <- centroids) yield (centroid, space.distance(x.projected, centroid))
           val nearestCentroid = distances.minBy(_._2)._1
           (nearestCentroid, x)
         }
       val grouped = for (centroid <- centroids) yield labeled.collect({
         case (`centroid`, x) => x
       })
-      val replacements = grouped.map(group => g.centroid(group.map(_.projected)))
+      val replacements = grouped.map(group => space.centroid(group.map(_.projected)))
       if (replacements.forall(replacement => centroids.exists(centroid => centroid == replacement))) {
         grouped
       } else {
         step(xs, replacements)
       }
     }
-    val projectedXs = xs.map(Pair)
-    val groupedMappings = step(projectedXs, projectedXs.take(k).map(_.projected))
-    groupedMappings.map(_.map(_.original))
+    val associated = xs.map(Pair)
+    val initial = pickRandom(associated.map(_.projected), k)
+    step(associated, initial).map(_.map(_.original))
   }
 
-  def cluster[T,U](fn: T => U)(xs: Seq[T], k: Int)(implicit g: VectorSpace[U]): Seq[Seq[T]] =
-    cluster(xs, k)(fn, g)
+  def cluster[T,U](fn: T => U)(xs: Seq[T], k: Int)
+                  (implicit g: VectorSpace[U]): Seq[Seq[T]] = cluster(xs, k)(fn, g)
 
 }
